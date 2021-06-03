a9d53f26c57b8200d588398cf9e8adb720c152ce
DANKoOoO <danko.97@hotmail.com>
2021-06-03 14:59:15 +0200

    Sredjene tabele u klasi GlavnaForma i dodate delimicno funkcionalnosti dugmicima. U klasi Kontroler dodate prazne metode koje se izvrsavaju na klik dugmeta.

--- src/main/java/rs/ac/bg/fon/ai/ZavrsniProjekat/Kontroler/Kontroler.java
+++ src/main/java/rs/ac/bg/fon/ai/ZavrsniProjekat/Kontroler/Kontroler.java
@@ -1,5 +1,15 @@
 package rs.ac.bg.fon.ai.ZavrsniProjekat.Kontroler;
 
-public class Kontroler {
+import rs.ac.bg.fon.ai.ZavrsniProjekat.Domen.Grad;
 
+public class Kontroler {
+	public void SacuvajFestival(String nazivFestivala, String datumPocetka, String datumZavrsetka, Grad grad) 
+	{
+		
+	}
+	public void PretraziFestivale(String deoImena) 
+	{
+		
+		
+	}
 }
