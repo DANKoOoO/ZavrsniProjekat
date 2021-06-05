44cd71d1babce26f194e1d17d937b2ea1775dfc8
DANKoOoO <danko.97@hotmail.com>
2021-06-05 15:44:10 +0200

    Prekopirane klase Grad i test Grad iz test grane.

--- src/main/java/rs/ac/bg/fon/ai/ZavrsniProjekat/Domen/Grad.java
+++ src/main/java/rs/ac/bg/fon/ai/ZavrsniProjekat/Domen/Grad.java
@@ -9,18 +9,33 @@ public class Grad {
 		return gradID;
 	}
 	public void setGradID(int gradID) {
+		if(gradID < 0) {
+			throw new RuntimeException("ID mora biti pozitivan ceo broj");
+		}
 		this.gradID = gradID;
 	}
 	public String getNaziv() {
 		return naziv;
 	}
 	public void setNaziv(String naziv) {
+		if(naziv == null) {
+			throw new NullPointerException("Grad ne sme biti null");
+		}
+		if((naziv.length() == 0)){
+			throw new RuntimeException("Grad mora da ima barem 1 slovo");
+		}
 		this.naziv = naziv;
 	}
 	public String getDrzava() {
 		return drzava;
 	}
 	public void setDrzava(String drzava) {
+		if(naziv == null) {
+			throw new NullPointerException("Drzava ne sme biti null");
+		}
+		if((naziv.length() == 0)){
+			throw new RuntimeException("Drzava mora da ima barem 1 slovo");
+		}
 		this.drzava = drzava;
 	}
 	@Override
@@ -33,6 +48,43 @@ public class Grad {
 		this.gradID = gradID;
 		this.naziv = naziv;
 		this.drzava = drzava;
+	}
+	public Grad() {
+		super();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(obj == null) {
+			return false;
+		}
+		if(getClass() != obj.getClass()) {
+			return false;
+		}
+		Grad gr = (Grad) obj;
+		if(naziv == null) {
+			if(gr.naziv != null) {
+				return false;
+			}
+			else if(!naziv.equals(gr.naziv)) {
+				return false;
+			}
+		}
+		if(drzava == null) {
+			if(gr.drzava != null) {
+				return false;
+			}
+			else if(!drzava.equals(gr.drzava)) {
+				return false;
+			}
+		}
+		if(gradID != gr.gradID) {
+			return false;
+		}
+
+		return true;
 	}
 	
 	
