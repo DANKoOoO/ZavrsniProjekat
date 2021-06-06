6d423bbe4eea2b1f8049e104c5a7aaec6a956b7c
DANKoOoO <danko.97@hotmail.com>
2021-06-06 12:46:39 +0200

    Dokumentovana klasa Glumac.

--- src/main/java/rs/ac/bg/fon/ai/ZavrsniProjekat/Domen/Glumac.java
+++ src/main/java/rs/ac/bg/fon/ai/ZavrsniProjekat/Domen/Glumac.java
@@ -2,26 +2,69 @@ package rs.ac.bg.fon.ai.ZavrsniProjekat.Domen;
 
 import javax.management.RuntimeErrorException;
 
+/**
+ * Klasa koja predstavlja Glumca.
+ * 
+ * Glumac ima id glumca i id filma kao Integer vrednosti.
+ * Glumac ima ime i prezime kao String vrednost.
+ * 
+ * @author danko
+ * @version 0.0.0.1
+ */
+
 public class Glumac {
+	/**
+	 * Id glumca kao Integer.
+	 */
 	private int glumacID;
+	/**
+	 * Ime i prezime glumca kao String.
+	 */
 	private String imePrezime;
+	/**
+	 * Id filma kao Integer.
+	 */
 	private int filmID;
 	
+	/**
+	 * Vraca id glumca.
+	 * 
+	 * @return Id glumca kao Integer.
+	 */
 	public int getGlumacID() {
 		return glumacID;
 	}
 	
+	/**
+	 * Postavlja id glumca na novu vrednost.
+	 * 
+	 * @param glumacID Id glumca kao Integer.
+	 * 
+	 * @throws java.lang.RuntimeException Ako je unet negativan broj.
+	 */
 	public void setGlumacID(int glumacID) {
 		if(glumacID < 0) {
 			throw new RuntimeException("ID mora biti pozitivan ceo broj");
 		}
 		this.glumacID = glumacID;
 	}
-	
+	/**
+	 * Vraca ime i prezime glumca.
+	 * 
+	 * @return Ime i prezime glumca kao String.
+	 */
 	public String getImePrezime() {
 		return imePrezime;
 	}
-	
+	/**
+	 * Postavlja ime i prezime glumca na novu vrednost.
+	 * 
+	 * @param imePrezime Ime i prezime glumca kao String.
+	 * 
+	 * @throws java.lang.NullPointerException Ako je uneta vrednost parametra imePrezime null.
+	 * @throws java.lang.RuntimeException Ako je unet String bez razmaka.
+	 * @throws java.lang.RuntimeException Ako je unet String koji sadrzi cifre.
+	 */
 	public void setImePrezime(String imePrezime) {
 		if(imePrezime == null) {
 			throw new NullPointerException("Ime i prezime ne sme biti null");
@@ -37,11 +80,22 @@ public class Glumac {
 		}
 		this.imePrezime = imePrezime;
 	}
-	
+	/**
+	 * Vraca id filma.
+	 * 
+	 * @return Id filma kao Integer.
+	 */
 	public int getFilmID() {
 		return filmID;
 	}
 	
+	/**
+	 * Postavlja id filma na novu vrednost.
+	 * 
+	 * @param filmID Id filma kao Integer.
+	 * 
+	 * @throws java.lang.RuntimeException Ako je unet negativan broj.
+	 */
 	public void setFilmID(int filmID) {
 		if(filmID < 0) {
 			throw new RuntimeException("ID mora biti pozitivan ceo broj");
@@ -49,6 +103,14 @@ public class Glumac {
 		this.filmID = filmID;
 	}
 	
+	/**
+	 * Konstruktor koji inicijalizuje objekat i postavlja vrednosti 
+	 * za id grada, ime i prezime glumca i id filma.
+	 * 
+	 * @param glumacID Id glumca kao Integer.
+	 * @param imePrezime Ime i prezime glumca kao String.
+	 * @param filmID Id filma kao Integer.
+	 */
 	public Glumac(int glumacID, String imePrezime, int filmID) {
 		super();
 		setGlumacID(glumacID);
@@ -56,15 +118,33 @@ public class Glumac {
 		setFilmID(filmID);;
 	}
 	
+	/**
+	 * Vraca formatiran String koji sadrzi samo ime i prezime glumca.
+	 * 
+	 * @return Ime i prezime glumca kao String.
+	 */
 	@Override
 	public String toString() {
 		return imePrezime;
 	}
 
+	/**
+	 * Konstruktor koji samo inicijalizuje objekat.
+	 */
 	public Glumac() {
 		super();
 	}
 
+	/**
+	 * Poredi dva glumca i vraca true ako su isti, a false ako nisu.
+	 * 
+	 * Glumci se porede po id-ju, imenu i prezimenu i id-ju filma.
+	 * 
+	 * <ul>
+	 * 		<li> true ako su oba objekta klase Glumac i imaju iste id-jeve, imena i prezimena i id-jeve filma.</li>
+	 * 		<li> false u svim ostalim slucajevima</li>
+	 * </ul>
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if(this == obj) {
